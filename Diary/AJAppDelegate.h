ca849d9465296ea0b976b23462cb821db355401b
Alberto Jauregui <bastertwo@gmail.com>
2014-05-26 17:04:20 -0600

    Initial Commit

---
+++ Diary/AJAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AJAppDelegate.h
+//  Diary
+//
+//  Created by Alberto Jauregui on 5/26/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
