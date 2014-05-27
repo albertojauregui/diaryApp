1955e0ec8930eec0feca7404cc5a8c93b1ce07d5
Alberto Jauregui <bastertwo@gmail.com>
2014-05-26 21:11:38 -0600

    Implementing fetch request for the entries on the diary

---
+++ Diary/AJEntryListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AJEntryListTableViewController.h
+//  Diary
+//
+//  Created by Alberto Jauregui on 5/26/14.
+//  Copyright (c) 2014 Alberto Jauregui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJEntryListTableViewController : UITableViewController
+
+@end
